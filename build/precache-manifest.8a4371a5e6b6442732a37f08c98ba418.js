b5fbf70873e3600481ca5b5424ca03f1ff224ea7
Sebastian Knudsen <knudsen.sebastian@gmail.com>
2020-01-18 14:43:34 +0100

    remove page

---
+++ build/precache-manifest.8a4371a5e6b6442732a37f08c98ba418.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf3d7abd77c63a1ed91dc2f554809da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15e9457c8c7e39e98e46",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b6fabe4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15e9457c8c7e39e98e46",
+    "url": "/static/js/main.f151277f.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sebdak.github.io/precache-manifest.0a7ce4439707d8e760f2175fcb266db0.js"
+  "/precache-manifest.8a4371a5e6b6442732a37f08c98ba418.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sebdak.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
